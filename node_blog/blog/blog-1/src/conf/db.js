13396a0fff81d5e4e967ca9b55b8c228fb168f98
niutong_bxbank <niutongoh@163.com>
2020-09-04 18:14:08 +0800

    nodejs连接redis

--- node_blog/blog/blog-1/src/conf/db.js
+++ node_blog/blog/blog-1/src/conf/db.js
@@ -2,7 +2,9 @@ const env = process.env.NODE_ENV //环境变量
 
 // 配置
 let MYSQL_CONF
+let REDIS_CONF
 if (env === 'dev') {
+  // mysql
   MYSQL_CONF = {
     host: '10.3.2.36',
     user: 'mysql_databus',
@@ -10,17 +12,29 @@ if (env === 'dev') {
     port: '3111',
     database: 'node_blog'
   }
+  // redis
+  REDIS_CONF = {
+    port: 6379,
+    host: '127.0.0.1'
+  }
 }
 if (env === 'production') {
+  // mysql
   MYSQL_CONF = {
     host: '10.3.2.36',
     user: 'mysql_databus',
     password: 'bFdY8UYTKjs0AqgEeAqQZBtPni',
     port: '3111',
     database: 'node_blog'
+  }
+  // redis
+  REDIS_CONF = {
+    port: 6379,
+    host: '127.0.0.1'
   }
 }
 
 module.exports = {
-  MYSQL_CONF
+  MYSQL_CONF,
+  REDIS_CONF
 }
